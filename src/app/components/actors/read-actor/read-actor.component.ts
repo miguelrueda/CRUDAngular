6b9e9ce285556373368f5d3ba781558948231444
greyss <greyss1@yahoo.es>
2019-01-31 18:43:25 -0600

    added changes for reusable component

--- src/app/components/actors/read-actor/read-actor.component.ts
+++ src/app/components/actors/read-actor/read-actor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-read-actor',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadActorComponent implements OnInit {
 
-  constructor() { }
+  mode = 'read';
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.parent.url.subscribe(urlPath => {
+      const url = urlPath[urlPath.length - 1].path;
+      console.log(url);
+      switch (url) {
+        case 'read':
+        this.mode = 'read';
+        break;
+        case 'update':
+        this.mode = 'update';
+        break;
+        case 'delete':
+        this.mode = 'delete';
+        break;
+      }
+    });
+    console.log(`mode is ${this.mode}`);
   }
 
 }
